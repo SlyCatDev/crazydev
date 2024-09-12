c1d7d43a162580cc14e85da1cfae55578b64c883
Enzo TESSIER <enzo.tessier@bts-malraux.net>
2024-09-12 15:08:04 +0200

    fichier vercel

--- api/app.js
+++ api/app.js
@@ -1,63 +1,83 @@
-import { MongoClient } from 'mongodb';
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./db');
+
+const app = express();
+const port = 3000;
+
+app.use(cors()); // Ajoute CORS pour toutes les requêtes
+app.use(express.json());
 
 let db;
 
-async function connectDB() {
-  if (db) return db;
-  const client = await MongoClient.connect('mongodb+srv://enzotessier:DjO3bR7V1dUluZ9Y@capeless-careers.ybowa.mongodb.net/Capeless-Careers', { useNewUrlParser: true, useUnifiedTopology: true });
-  db = client.db('Capeless-Careers');
-  return db;
-}
+connectDB().then(database => {
+    db = database;
+    app.listen(port, () => {
+        console.log(`Server is running on port ${port}`);
+    });
+});
 
-export default async function handler(req, res) {
-  const { method } = req;
-
-  try {
-    const database = await connectDB();
-    const collection = database.collection('metier');
-
-    switch (method) {
-      case 'GET':
-        if (req.query.search) {
-          const query = {};
-          if (req.query.securite) query.securite = parseInt(req.query.securite);
-          if (req.query.confort) query.confort = parseInt(req.query.confort);
-          if (req.query.creativite) query.creativite = parseInt(req.query.creativite);
-
-          const data = await collection.find(query).toArray();
-          if (data.length > 0) {
-            res.status(200).json(data);
-          } else {
-            res.status(404).send('Aucun métier trouvé');
-          }
-        } else {
-          const data = await collection.find({}).toArray();
-          res.status(200).json(data);
-        }
-        break;
-      case 'POST':
-        const { nom, description, securite, confort, creativite } = req.body;
-
-        if (!nom || !description || !securite || !confort || !creativite) {
-          return res.status(400).send('Tous les champs sont requis');
-        }
-
-        const metier = {
-          nom,
-          description,
-          securite: parseInt(securite),
-          confort: parseInt(confort),
-          creativite: parseInt(creativite)
-        };
-
-        const result = await collection.insertOne(metier);
-        res.status(201).json(result);
-        break;
-      default:
-        res.setHeader('Allow', ['GET', 'POST']);
-        res.status(405).end(`Method ${method} Not Allowed`);
+app.get('/metier', async (req, res) => {
+    try {
+        const collection = db.collection('metier');
+        const data = await collection.find({}).toArray();
+        res.json(data);
+    } catch (err) {
+        res.status(500).send(err);
     }
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-}
+});
+
+app.get('/metier/search', async (req, res) => {
+    const { securite, confort, creativite } = req.query;
+    const query = {};
+
+    if (securite) query.securite = parseInt(securite);
+    if (confort) query.confort = parseInt(confort);
+    if (creativite) query.creativite = parseInt(creativite);
+
+    try {
+        const collection = db.collection('metier');
+        const data = await collection.find(query).toArray();
+        if (data.length > 0) {
+            res.json(data);
+        } else {
+            res.status(404).send('Aucun métier trouvé');
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+app.get('/questions', async (req, res) => {
+    try {
+        const collection = db.collection('question');
+        const data = await collection.find({}).toArray();
+        res.json(data);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
+
+app.post('/metier', async (req, res) => {
+    const { nom, description, securite, confort, creativite } = req.body;
+
+    if (!nom || !description || !securite || !confort || !creativite) {
+        return res.status(400).send('Tous les champs sont requis');
+    }
+
+    const metier = {
+        nom,
+        description,
+        securite: parseInt(securite),
+        confort: parseInt(confort),
+        creativite: parseInt(creativite)
+    };
+
+    try {
+        const collection = db.collection('metier');
+        const result = await collection.insertOne(metier);
+        res.json(result);
+    } catch (err) {
+        res.status(500).send(err);
+    }
+});
